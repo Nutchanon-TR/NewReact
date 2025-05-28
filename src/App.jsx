330482ddf15a2caa049f9821ef20ea8b1d01493d
Nutchanon-TR <nutchanon.charo@mail.kmutt.ac.th>
2025-05-29 00:10:44 +0700

    default react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import './css/index.css';
+
+function App() {
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+      {/* <Link to="/">App</Link><br />
+
+      <Routes>
+          <Route path="/" element={<App/>} />
+      </Routes> */}
+    </>
+  )
+}
+
+export default App
